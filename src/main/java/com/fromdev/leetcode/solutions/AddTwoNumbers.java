899fd9bc6f83e4321ff25b2d1abf8ebe2c362c57
Sachin, Joshi(sacjoshi) <sacjoshi@ebay.com>
2018-06-30 00:41:42 -0700

    forum answer added

--- src/main/java/com/fromdev/leetcode/solutions/AddTwoNumbers.java
+++ src/main/java/com/fromdev/leetcode/solutions/AddTwoNumbers.java
@@ -3,9 +3,9 @@ package com.fromdev.leetcode.solutions;
 public class AddTwoNumbers {
     public static void main(String[] args) {
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
-        ListNode l1 = newListNode(9004);
+        ListNode l1 = newListNode(Integer.MAX_VALUE);
         ListNode l2 = newListNode(119876);
-        ListNode sum = addTwoNumbers.addTwoNumbers(l1,l2);
+        ListNode sum = addTwoNumbers.addTwoNumbers2(l1,l2);
         print(l1);
         System.out.print(" + ");
         print(l2);
@@ -50,6 +50,25 @@ public class AddTwoNumbers {
             sum.next = caryOverNode;
         }
         return sum;
+    }
+
+    //from forum answer
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            ListNode cur = new ListNode(0);
+            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+        return head.next;
     }
 }
 
