47c53f89b42cf6de1ce84712d9c623f05a873318
Sachin, Joshi(sacjoshi) <sacjoshi@ebay.com>
2018-08-27 12:05:08 -0700

    Median of sorted arrays solution added - accepted answer

---
+++ src/main/java/com/fromdev/leetcode/solutions/MedianSortedArrays.java
@@ -0,0 +1,31 @@
+package com.fromdev.leetcode.solutions;
+
+import java.util.Arrays;
+
+public class MedianSortedArrays {
+
+    public static void main(String[] args) {
+        MedianSortedArrays solution = new MedianSortedArrays();
+        int num1[] = {1,3};
+        int num2[] = {2};
+        System.out.println(solution.findMedianSortedArrays(num1,num2));
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int all [] = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1,0,all,0,nums1.length);
+        System.arraycopy(nums2,0,all,nums1.length,nums2.length);
+        Arrays.sort(all);
+      //  printArray(all);
+        double median = (all.length % 2 == 0) ? (all[all.length / 2 - 1] + all[(all.length / 2)]) / 2.0d : all[all.length/2];
+        return median;
+    }
+
+    private void printArray(int[] arr) {
+        for(int i: arr) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+}
